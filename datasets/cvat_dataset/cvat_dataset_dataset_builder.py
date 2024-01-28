6fce73b85c1e401ea4f9ca2ec8a290c78009a04d
Alwin Zomotor <inf21034@lehre.dhbw-stuttgart.de>
2024-01-28 21:43:11 +0100

    reformatted cvat dataset

--- datasets/cvat_dataset/cvat_dataset_dataset_builder.py
+++ datasets/cvat_dataset/cvat_dataset_dataset_builder.py
@@ -11,88 +11,89 @@ import os
 import math
 
 cnt_unique = 0
+
+
 class Builder(tfds.core.GeneratorBasedBuilder):
-  """DatasetBuilder for cvat_dataset dataset."""
+    """DatasetBuilder for cvat_dataset dataset."""
 
-  VERSION = tfds.core.Version('1.3.0')
-  RELEASE_NOTES = {
-      '1.3.0': 'BEV fixed',
-  }
-
-  def _info(self) -> tfds.core.DatasetInfo:
-    """Returns the dataset metadata."""
-    # TODO(cvat_dataset): Specifies the tfds.core.DatasetInfo object
-    return self.dataset_info_from_configs(
-        # builder=self,
-
-        description="CVat Lane Dataset",
-
-        features=tfds.features.FeaturesDict({
-            "image": tfds.features.Tensor(dtype=tf.float32, shape=(None, None, 3), encoding="bytes"),
-            "label": tfds.features.Sequence(tfds.features.Tensor(shape=(None, None), dtype=tf.float32, encoding="bytes")),
-            # "h_samples": tfds.features.Sequence(tfds.features.Tensor(shape=(None,), dtype=tf.int32)),
-            # "augmentation_index": tf.int32,
-        }),
-        supervised_keys=("image", "label"),
-    )
-
-  def _split_generators(self, dl_manager: tfds.download.DownloadManager):
-    """Returns SplitGenerators."""
-    # TODO(cvat_dataset): Downloads the data and defines the splits
-    # path = dl_manager.download_and_extract('https://todo-data-url')
-    path = "/mnt/c/Users/inf21034/source/IMG_ROOTS/1280x960_CVATROOT/"
-    # TODO(cvat_dataset): Returns the Dict[split names, Iterator[Key, Example]]
-    return {
-        'train': self._generate_examples(os.path.join(path, "train_set"),
-                                         'train_set.json',
-                                         [0.0, -15.0, 15.0, -30.0, 30.0]),
-        'test': self._generate_examples(os.path.join(path, "test_set"), 'test_set.json', None),
+    VERSION = tfds.core.Version('1.3.0')
+    RELEASE_NOTES = {
+        '1.3.0': 'BEV fixed',
     }
 
-  def _generate_examples(self, path, label_data_name, augmentation_deg):
-    """Yields examples."""
-    # DONE(cvat_dataset): Yields (key, example) tuples from the dataset
-    global cnt_unique
-    with open("/mnt/c/Users/inf21034/PycharmProjects/edge_tpu_lane_detection/add_ins/cvat_config2.json") as json_file:
-        config = json.load(json_file)
-    net_input_img_size = config["model_info"]["input_image_size"]
-    x_anchors = config["model_info"]["x_anchors"]
-    y_anchors = config["model_info"]["y_anchors"]
-    max_lane_count = config["model_info"]["max_lane_count"]
-    ground_img_size = config["model_info"]["input_image_size"]
+    def _info(self) -> tfds.core.DatasetInfo:
+        """Returns the dataset metadata."""
+        return self.dataset_info_from_configs(
+            # builder=self,
 
-    H_list, map_x_list, map_y_list = create_map(config,
-                                                augmentation_deg=augmentation_deg)
-    H_list = tf.constant(H_list)
-    map_x_list = tf.constant(map_x_list)
-    map_y_list = tf.constant(map_y_list)
+            description="CVat Lane Dataset",
 
-    for image_ary, label_lanes, label_h_samples, refIdx in _data_reader(path, label_data_name,
-                                                                        augmentation_deg):
-        # Generate a unique key for each example
-        key = cnt_unique
-        cnt_unique += 1
-        img, label = _map_projection_data_generator(image_ary,
-                                                    label_lanes,
-                                                    label_h_samples,
-                                                    net_input_img_size,
-                                                    x_anchors,
-                                                    y_anchors,
-                                                    max_lane_count,
-                                                    H_list[refIdx],
-                                                    map_x_list[refIdx],
-                                                    map_y_list[refIdx],
-                                                    ground_img_size
-                                                    )
-        yield key, {
-            "image": img,
-            "label": label,
+            features=tfds.features.FeaturesDict({
+                "image": tfds.features.Tensor(dtype=tf.float32, shape=(None, None, 3), encoding="bytes"),
+                "label": tfds.features.Sequence(
+                    tfds.features.Tensor(shape=(None, None), dtype=tf.float32, encoding="bytes")),
+                # "h_samples": tfds.features.Sequence(tfds.features.Tensor(shape=(None,), dtype=tf.int32)),
+                # "augmentation_index": tf.int32,
+            }),
+            supervised_keys=("image", "label"),
+        )
+
+    def _split_generators(self, dl_manager: tfds.download.DownloadManager):
+        """Returns SplitGenerators."""
+        path = "/mnt/c/Users/inf21034/source/IMG_ROOTS/1280x960_CVATROOT/"
+        return {
+            'train': self._generate_examples(os.path.join(path, "train_set"),
+                                             'train_set.json',
+                                             [0.0, -15.0, 15.0, -30.0, 30.0]),
+            'test': self._generate_examples(os.path.join(path, "test_set"), 'test_set.json', None),
         }
-    # for f in path.glob('*.jpeg'):
-    #   yield 'key', {
-    #       'image': f,
-    #       'label': 'yes',
-    #   }
+
+    def _generate_examples(self, path, label_data_name, augmentation_deg):
+        """Yields examples."""
+        # DONE(cvat_dataset): Yields (key, example) tuples from the dataset
+        global cnt_unique
+        with open(
+                "/mnt/c/Users/inf21034/PycharmProjects/edge_tpu_lane_detection/add_ins/cvat_config2.json") as json_file:
+            config = json.load(json_file)
+        net_input_img_size = config["model_info"]["input_image_size"]
+        x_anchors = config["model_info"]["x_anchors"]
+        y_anchors = config["model_info"]["y_anchors"]
+        max_lane_count = config["model_info"]["max_lane_count"]
+        ground_img_size = config["model_info"]["input_image_size"]
+
+        H_list, map_x_list, map_y_list = create_map(config,
+                                                    augmentation_deg=augmentation_deg)
+        H_list = tf.constant(H_list)
+        map_x_list = tf.constant(map_x_list)
+        map_y_list = tf.constant(map_y_list)
+
+        for image_ary, label_lanes, label_h_samples, refIdx in _data_reader(path, label_data_name,
+                                                                            augmentation_deg):
+            # Generate a unique key for each example
+            key = cnt_unique
+            cnt_unique += 1
+            img, label = _map_projection_data_generator(image_ary,
+                                                        label_lanes,
+                                                        label_h_samples,
+                                                        net_input_img_size,
+                                                        x_anchors,
+                                                        y_anchors,
+                                                        max_lane_count,
+                                                        H_list[refIdx],
+                                                        map_x_list[refIdx],
+                                                        map_y_list[refIdx],
+                                                        ground_img_size
+                                                        )
+            yield key, {
+                "image": img,
+                "label": label,
+            }
+        # for f in path.glob('*.jpeg'):
+        #   yield 'key', {
+        #       'image': f,
+        #       'label': 'yes',
+        #   }
+
 
 def _data_reader(dataset_path,
                  label_data_name,
@@ -103,7 +104,7 @@ def _data_reader(dataset_path,
         print("Label file doesn't exist, path : ", label_data_path)
         sys.exit(0)
 
-    count =0
+    count = 0
 
     # load data
     with open(label_data_path, 'r') as reader:
@@ -127,6 +128,7 @@ def _data_reader(dataset_path,
             else:
                 for refIdx in range(len(augmentation_deg)):
                     yield image_ary, label_lanes, label_h_samples, refIdx
+
 
 def create_map(config,
                augmentation_deg=None):
@@ -173,8 +175,8 @@ def create_map(config,
     #     ################################
     #
     for i in range(len(groundP)):
-        groundP[i][0] = groundP[i][0] * gw/w # ground_scale_width + gw / 2.0
-        groundP[i][1] = groundP[i][1] * gh/h # gh - groundP[i][1] * ground_scale_height
+        groundP[i][0] = groundP[i][0] * gw / w  # ground_scale_width + gw / 2.0
+        groundP[i][1] = groundP[i][1] * gh / h  # gh - groundP[i][1] * ground_scale_height
 
     list_H = []
     list_map_x = []
@@ -215,17 +217,18 @@ def create_map(config,
 
     return list_H, list_map_x, list_map_y
 
+
 def _map_projection_data_generator(src_image,
-                                     label_lanes,
-                                     label_h_samples,
-                                     net_input_img_size,
-                                     x_anchors,
-                                     y_anchors,
-                                     max_lane_count,
-                                     H,
-                                     map_x,
-                                     map_y,
-                                     groundSize):
+                                   label_lanes,
+                                   label_h_samples,
+                                   net_input_img_size,
+                                   x_anchors,
+                                   y_anchors,
+                                   max_lane_count,
+                                   H,
+                                   map_x,
+                                   map_y,
+                                   groundSize):
     # transform image by perspective matrix
     # height, width = src_image.shape[:2]
     # if width != 1280 or height != 720:
@@ -234,13 +237,13 @@ def _map_projection_data_generator(src_image,
     gImg = cv2.remap(src_image, np.array(map_x), np.array(map_y),
                      interpolation=cv2.INTER_NEAREST,
                      borderValue=(125, 125, 125))
-    imgf = np.float32(gImg) * (1.0/ 255.0)
+    imgf = np.float32(gImg) * (1.0 / 255.0)
     # cv2.imwrite("test.jpg", gImg)
 
     # create label for class
-    class_list= {'background'         : [0, 1],
-                 'lane_marking'       : [1, 0]}
-    class_count = len(class_list)     # [background, road]
+    class_list = {'background': [0, 1],
+                  'lane_marking': [1, 0]}
+    class_count = len(class_list)  # [background, road]
 
     # create label for slice id mapping from  ground x anchor, and y anchor
     #   [y anchors,
@@ -257,9 +260,8 @@ def _map_projection_data_generator(src_image,
     instance_label_idx = class_count + offset_dim
 
     # init values
-    label[:,:,class_idx:class_idx+class_count] = class_list['background']
-    label[:,:,x_offset_idx] = 0.0001
-
+    label[:, :, class_idx:class_idx + class_count] = class_list['background']
+    label[:, :, x_offset_idx] = 0.0001
 
     # transform "h_samples" & "lanes" to desired format
     anchor_scale_x = float(x_anchors) / float(groundSize[1])
@@ -288,18 +290,17 @@ def _map_projection_data_generator(src_image,
             # conver to anchor coordinate(grid)
             gx = int(gx / gz)
             gy = int(gy / gz)
-            if gx < 0 or gy < 0 or gx >=(groundSize[1]-1) or gy >= (groundSize[0]-1):
+            if gx < 0 or gy < 0 or gx >= (groundSize[1] - 1) or gy >= (groundSize[0] - 1):
                 continue
 
             ax = int(gx * anchor_scale_x)
             ay = int(gy * anchor_scale_y)
 
-            if ax < 0 or ay < 0 or ax >=(x_anchors-1) or ay >= (y_anchors-1):
+            if ax < 0 or ay < 0 or ax >= (x_anchors - 1) or ay >= (y_anchors - 1):
                 continue
 
             instance_label_value = (laneIdx + 1.0) * 50
-            label[ay][ax][class_idx:class_idx+class_count] = class_list['lane_marking']
-
+            label[ay][ax][class_idx:class_idx + class_count] = class_list['lane_marking']
 
             # do line interpolation to padding label data for perspectived coordinate.
             if prev_gx is None:
@@ -309,12 +310,12 @@ def _map_projection_data_generator(src_image,
                 prev_ay = ay
             else:
                 if abs(ay - prev_ay) <= 1:
-                    if acc_count [ay][ax][x_offset_idx] > 0:
+                    if acc_count[ay][ax][x_offset_idx] > 0:
                         continue
                     offset = gx - (ax / anchor_scale_x)
-                    label[ay][ax][x_offset_idx] += math.log(offset+0.0001)
+                    label[ay][ax][x_offset_idx] += math.log(offset + 0.0001)
                     label[ay][ax][instance_label_idx] = instance_label_value
-                    acc_count [ay][ax][x_offset_idx] = 1
+                    acc_count[ay][ax][x_offset_idx] = 1
                 else:
                     gA = np.array([prev_gx, prev_gy])
                     gB = np.array([gx, gy])
@@ -329,14 +330,14 @@ def _map_projection_data_generator(src_image,
                         ax = np.int32(gC[0] * anchor_scale_x)
                         ay = np.int32(gC[1] * anchor_scale_y)
 
-                        if acc_count [ay][ax][x_offset_idx] > 0:
+                        if acc_count[ay][ax][x_offset_idx] > 0:
                             continue
 
                         offset = gC[0] - (ax / anchor_scale_x)
-                        label[ay][ax][x_offset_idx] += math.log(offset+0.0001)
-                        label[ay][ax][class_idx:class_idx+class_count] = class_list['lane_marking']
+                        label[ay][ax][x_offset_idx] += math.log(offset + 0.0001)
+                        label[ay][ax][class_idx:class_idx + class_count] = class_list['lane_marking']
                         label[ay][ax][instance_label_idx] = instance_label_value
-                        acc_count [ay][ax][x_offset_idx] = 1
+                        acc_count[ay][ax][x_offset_idx] = 1
 
                 prev_gx = gx
                 prev_gy = gy
